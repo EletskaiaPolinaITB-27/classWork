9419cf26579cc046713aa0260c7534d077e2da6f
Student <student@compass.local>
2025-12-16 12:18:39 +0600

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-const { createElement } = require("react")
-
 const btn = document.getElementById('button')
 
 const customAlert = (message) => {
-    const overlay = document.createElement('div')
+    const overlay = document.createElement('div');
     overlay.className = 'overlay';
-
     document.body.append(overlay);
+
+    const contentDiv = document.createElement('div');
+    contentDiv.className = 'content';
+    overlay.append(contentDiv);
+
+    const messageH1 = document.createElement('h1');
+    messageH1.textContent = message;
+    contentDiv.append(messageH1);
+
+    const closeButton = document.createElement('button');
+    closeButton.className = 'closeBtn';
+    closeButton.textContent = 'oke';
+    contentDiv.append(closeButton);
+
+    closeButton.addEventListener('click', () => {
+        overlay.remove();
+    })
+    overlay.addEventListener('click', (event) =>{
+        if (event.target.className === 'overlay'){
+            overlay.remove();
+        }
+    })
 }
 btn.addEventListener('click', () => {
-    customAlert('learn alert')
+    customAlert('learn frontend')
+})
+
+button2.addEventListener('click', () =>{
+    customAlert('do homework')
+})
+
+button3.addEventListener('click', () =>{
+    customAlert('go to sleep')
 })
